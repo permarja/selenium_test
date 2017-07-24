4e18a5bb89d3e1824a8dfe8c78806799a52f1e74
Javier Perez Martin <permarja@gmail.com>
2017-07-24 12:44:19 +0200

    sdf

--- src/test/java/loadtesting/GoogleHomePageTest.java
+++ src/test/java/loadtesting/GoogleHomePageTest.java
@@ -1,33 +1,40 @@
 package loadtesting;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class GoogleHomePageTest {
-	
-	private WebDriver driver; 
-	String appURL = "http://google.com";
 
-	@BeforeClass
-	public void testSetUp() {
-		
-		driver = new FirefoxDriver();
-	}
-	
-	@Test
-	public void verifyGooglePageTittle() {
-		driver.navigate().to(appURL);
-		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "Google");
-	}
-	
-	@AfterClass
-	public void tearDown() {
-		driver.quit();
-	}
-	
+    private WebDriver driver;
+    String appURL = "http://google.com";
+
+    @BeforeClass
+    public void testSetUp() throws MalformedURLException {
+
+        //driver = new FirefoxDriver();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+    }
+
+    @Test
+    public void verifyGooglePageTittle() {
+        //driver.navigate().to(appURL);
+        //String getTitle = driver.getTitle();
+        //Assert.assertEquals(getTitle, "Google");
+        driver.get("http://www.google.es");
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
